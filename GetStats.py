60c76d3a7378274afefad793d683e506f467c5f9
ShaelShah <shael.shah7@gmail.com>
2017-01-27 17:35:38 -0500

    Grouped similar stats together

--- GetStats.py
+++ GetStats.py
@@ -1,9 +1,13 @@
 import nflgame
+import xml.etree.cElementTree as ET
 
-weeks = [1, 2, 3]
+weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 yearStatLine = []
 
 for week in weeks:
+
+    print week
+
     game = nflgame.games(2016, week)
     players = nflgame.combine_play_stats(game)
 
@@ -11,12 +15,11 @@ for week in weeks:
         try:
             # General info
             playerInfo = []
-            playerInfo.append(p.player.player_id)
-            playerInfo.append(p.player.name)
-            playerInfo.append(p.team)
-            playerInfo.append(week)
-            playerInfo.append(p.home)
-            playerInfo.append(p.player.position)
+            playerInfo.extend([p.player.player_id, p.player.name, p.team, p.player.position])
+
+            # Player Weekly Info
+            playerWeekInfo = []
+            playerWeekInfo.extend([week, p.home])
 
             # Passing info
             playerPassing = []
@@ -36,41 +39,44 @@ for week in weeks:
             playerMisc = []
             playerMisc.extend([p.fumbles_tot, p.fumbles_lost])
 
-            playerStatLine = [playerInfo, playerPassing, playerReceiving, playerRushing, playerMisc]
+            playerStatLine = [playerInfo, playerWeekInfo, playerPassing, playerReceiving, playerRushing, playerMisc]
             yearStatLine.append(playerStatLine)
 
         except AttributeError:
-            # print p.name
+            print str("Could not find info for ") + p.name
             pass
 
 yearStatLine.sort()
+for index in yearStatLine:
+    print index
 
-print yearStatLine
+player = ET.Element("Player")
+id = ET.SubElement(player, "ID")
 
-# length = len(yearStatLine)
 
-# for index in length:
-#     if yearStatLine[index][0] == yearStatLine[index + 1][0]:
-#         catList = yearStatLine[index] + yearStatLine[index + 1]
-#
-#
 # print yearStatLine
-
-
 
 # nflgame.combine(nflgame.games(2016)).csv('season2010.csv')
 
-# sys.stdout.write(str(p.player.player_id) + ',' + str(p.player.name) + ',' + str(p.team) + ',' + str(week) + ',' + str(p.home) + ',' + str(p.player.position) + '*')
-# statLine = (str(p.player.player_id) + ',' + str(p.player.name) + ',' + str(p.team) + ',' + str(week) + ',' + str(p.home) + ',' + str(p.player.position) + '*')
+# sys.stdout.write(str(p.player.player_id) + ',' + str(p.player.name) + ',' + str(p.team) + ',' + str(week) + ','
+    # + str(p.home) + ',' + str(p.player.position) + '*')
+# statLine = (str(p.player.player_id) + ',' + str(p.player.name) + ',' + str(p.team) + ',' + str(week) + ','
+    # + str(p.home) + ',' + str(p.player.position) + '*')
 
-# sys.stdout.write(str(p.passing_att) + ',' + str(p.passing_cmp) + ',' + str(p.passing_yds) + ',' + str(p.passing_int) + ',' + str(p.passing_tds) + ',' + str(p.twopta) + ',' + str(p.twoptm) + ',')
-# statLine += (str(p.passing_att) + ',' + str(p.passing_cmp) + ',' + str(p.passing_yds) + ',' + str(p.passing_int) + ',' + str(p.passing_tds) + ',' + str(p.twopta) + ',' + str(p.twoptm) + ',')
+# sys.stdout.write(str(p.passing_att) + ',' + str(p.passing_cmp) + ',' + str(p.passing_yds) + ',' + str(p.passing_int)
+    # + ',' + str(p.passing_tds) + ',' + str(p.twopta) + ',' + str(p.twoptm) + ',')
+# statLine += (str(p.passing_att) + ',' + str(p.passing_cmp) + ',' + str(p.passing_yds) + ',' + str(p.passing_int)
+    # + ',' + str(p.passing_tds) + ',' + str(p.twopta) + ',' + str(p.twoptm) + ',')
 
-# sys.stdout.write(str(p.receiving_tar) + ',' + str(p.receiving_rec) + ',' + str(p.receiving_yds) + ',' + str(p.receiving_yac_yds) + ',' + str(p.receiving_tds) + ',' + str(p.twoptm) + ',')
-# statLine += (str(p.receiving_tar) + ',' + str(p.receiving_rec) + ',' + str(p.receiving_yds) + ',' + str(p.receiving_yac_yds) + ',' + str(p.receiving_tds) + ',' + str(p.twoptm) + ',')
+# sys.stdout.write(str(p.receiving_tar) + ',' + str(p.receiving_rec) + ',' + str(p.receiving_yds) + ','
+    # + str(p.receiving_yac_yds) + ',' + str(p.receiving_tds) + ',' + str(p.twoptm) + ',')
+# statLine += (str(p.receiving_tar) + ',' + str(p.receiving_rec) + ',' + str(p.receiving_yds) + ','
+    # + str(p.receiving_yac_yds) + ',' + str(p.receiving_tds) + ',' + str(p.twoptm) + ',')
 
-# sys.stdout.write(str(p.rushing_att) + ',' + str(p.rushing_yds) + ',' + str(p.rushing_rac_yds) + ',' + str(p.rushing_tds) + ',' + str(p.twoptm) + ',')
-# statLine += (str(p.rushing_att) + ',' + str(p.rushing_yds) + ',' + str(p.rushing_rac_yds) + ',' + str(p.rushing_tds) + ',' + str(p.twoptm) + ',')
+# sys.stdout.write(str(p.rushing_att) + ',' + str(p.rushing_yds) + ',' + str(p.rushing_rac_yds) + ','
+    # + str(p.rushing_tds) + ',' + str(p.twoptm) + ',')
+# statLine += (str(p.rushing_att) + ',' + str(p.rushing_yds) + ',' + str(p.rushing_rac_yds) + ','
+    # + str(p.rushing_tds) + ',' + str(p.twoptm) + ',')
 
 # print(str(p.fumbles_tot) + ',' + str(p.fumbles_lost))
 # statLine += (str(p.fumbles_tot) + ',' + str(p.fumbles_lost))
